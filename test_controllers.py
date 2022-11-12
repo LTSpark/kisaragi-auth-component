ae2c4da2344653b8f822f6b8a1793275c1116ef7
Leonardo Torres <dleonardotb2001@gmail.com>
2022-11-12 09:19:49 -0500

    fix test

--- test_controllers.py
+++ test_controllers.py
@@ -1,78 +1,81 @@
 from fastapi.testclient import TestClient
 from main import app
+
 client = TestClient(app)
 
+
 def test_create_user():
-  response = client.post(
-      "/api/v1/users",
-      json={
-          "user_name": "Edward Ramos",
-          "email": "edward.ramos30@gmail.com",
-          "password": "Edward123",
-          "birth_date": "2022-11-12",
-          "telephone_number": "123456789",
-          "role": "ADMIN"
-          }
-      )
-  assert response.status_code == 201
-  assert "user" in response.json()
+    response = client.post(
+        "/api/v1/users",
+        json={
+            "user_name": "Edward Ramos",
+            "email": "edward.ramos30@gmail.com",
+            "password": "Edward123",
+            "birth_date": "2022-11-12",
+            "telephone_number": "123456789",
+            "role": "ADMIN"
+        }
+    )
+    assert response.status_code == 201
+    assert "user" in response.json()
+
 
 def test_create_user_fail():
-  response = client.post(
-      "/api/v1/users"
-      )
-  assert response.status_code == 422
-
-
+    response = client.post(
+        "/api/v1/users"
+    )
+    assert response.status_code == 422
 
 
 def test_login_user():
-  response = client.post(
-      "/api/v1/auth",
-      json={
-          'email': "arian30@gmail.com",
-          'password': "Arian123"
-          }
-      )
-  assert response.status_code == 200
-  assert "user" in response.json()
+    response = client.post(
+        "/api/v1/auth",
+        json={
+            'email': "arian30@gmail.com",
+            'password': "Arian123"
+        }
+    )
+    assert response.status_code == 200
+    assert "user" in response.json()
+
 
 def test_login_user_fail1():
-  response = client.post(
-      "/api/v1/auth",
-      json={
-          'email': "arian29@gmail.com",
-          'password': "Arian123"
-          }
-      )
-  assert response.status_code == 404
+    response = client.post(
+        "/api/v1/auth",
+        json={
+            'email': "arian29@gmail.com",
+            'password': "Arian123"
+        }
+    )
+    assert response.status_code == 404
+
 
 def test_login_user_fail2():
-  response = client.post(
-      "/api/v1/auth"
-      )
-  assert response.status_code == 422
-
+    response = client.post(
+        "/api/v1/auth"
+    )
+    assert response.status_code == 422
 
 
 def test_get_user_by_email():
-  email = "arian30@gmail.com"
-  response = client.get(
-      f"/api/v1/users/{email}/email"
+    email = "arian30@gmail.com"
+    response = client.get(
+        f"/api/v1/users/{email}/email"
     )
-  assert response.status_code == 200
-  assert "user_id" in response.json()
+    assert response.status_code == 200
+    assert "user_id" in response.json()
+
 
 def test_get_user_by_email_fail():
-  email = ""
-  response = client.get(
-      f"/api/v1/users/{email}/email"
+    email = ""
+    response = client.get(
+        f"/api/v1/users/{email}/email"
     )
-  assert response.status_code == 404
-  
+    assert response.status_code == 404
+
 
 def test_create_user_health():
-  response = client.get(
-      "/api/v1/health"
+    response = client.get(
+        "/api/v1/health"
     )
-  assert response.json() == {"msg": "Everything is OK!", "status": "Healthy"}
+    assert response.json() == {"msg": "Everything is OK!", "status": "Healthy"}
