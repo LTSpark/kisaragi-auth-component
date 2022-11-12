2bcf1fa7641f6e2a616f03bb828d11f088b4da16
Leonardo Torres <dleonardotb2001@gmail.com>
2022-11-12 01:11:26 -0500

    fix: names on health controller

--- app/controllers/health_controller.py
+++ app/controllers/health_controller.py
@@ -4,5 +4,5 @@ health_router = APIRouter()
 
 
 @health_router.get("/api/v1/health")
-async def create_user():
+async def health_check():
     return {"msg": "Everything is OK!", "status": "Healthy"}
