74d39219b7f813ef78e7320528ca6ac543769b90
Leonardo Torres <dleonardotb2001@gmail.com>
2022-12-23 23:32:13 -0500

    Fix Health Controller

--- app/controllers/health_controller.py
+++ app/controllers/health_controller.py
@@ -1,9 +1,5 @@
 from fastapi import APIRouter
 
-import os
-
-from dotenv import load_dotenv
-
 
 load_dotenv()
 
@@ -12,4 +8,4 @@ health_router = APIRouter()
 
 @health_router.get("/api/v1/health")
 async def health_check():
-    return {"msg": "Everything is OK!", "status": "Healthy", "database": os.getenv("MONGODB_URI")}
+    return {"msg": "Everything is OK!", "status": "Healthy"}
